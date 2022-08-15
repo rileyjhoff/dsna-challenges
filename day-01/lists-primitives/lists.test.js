31e0cbeb13417434b914777150e2c564dd308a89
Riley Hoffman <rileyjhoff@gmail.com>
2022-08-15 12:04:11 -0700

    write fizzBuzz function and test

---
+++ day-01/lists-primitives/lists.test.js
@@ -0,0 +1,36 @@
+// CH03-fizz-buzz
+function fizzBuzz(number) {
+  const arr = [];
+  for (let i = 1; i <= number; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      arr.push('FizzBuzz');
+    } else if (i % 3 === 0) {
+      arr.push('Fizz');
+    } else if (i % 5 === 0) {
+      arr.push('Buzz');
+    } else arr.push(i);
+  }
+  return arr;
+}
+
+test('fizz buzz', () => {
+  const arr = fizzBuzz(16);
+  expect(arr).toEqual([
+    1,
+    2,
+    'Fizz',
+    4,
+    'Buzz',
+    'Fizz',
+    7,
+    8,
+    'Fizz',
+    'Buzz',
+    11,
+    'Fizz',
+    13,
+    14,
+    'FizzBuzz',
+    16,
+  ]);
+});

2d7423f0510e382ec0b32d9050c3ada41b053e84
Riley Hoffman <rileyjhoff@gmail.com>
2022-08-15 12:43:41 -0700

    write multipleOfN function and test

--- day-01/lists-primitives/lists.test.js
+++ day-01/lists-primitives/lists.test.js
@@ -1,3 +1,21 @@
+// CH-multiples-of-n
+function multiplesOfN(n) {
+  const list = [];
+  for (let i = 1; i <= 50; i++) {
+    if (i % n === 0) list.push(i);
+  }
+  return list;
+}
+
+test('multiplesOfN', () => {
+  const list1 = multiplesOfN(5);
+  expect(list1).toEqual([5, 10, 15, 20, 25, 30, 35, 40, 45, 50]);
+  const list2 = multiplesOfN(25);
+  expect(list2).toEqual([25, 50]);
+  const list3 = multiplesOfN(75);
+  expect(list3).toEqual([]);
+});
+
 // CH03-fizz-buzz
 function fizzBuzz(number) {
   const list = [];

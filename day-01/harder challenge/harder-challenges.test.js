123c5002acdc26c7b613403cedb40f137496d962
Riley Hoffman <rileyjhoff@gmail.com>
2022-08-15 15:24:17 -0700

    solve equal sides challenge

--- day-01/harder challenge/harder-challenges.test.js
+++ day-01/harder challenge/harder-challenges.test.js
@@ -32,3 +32,27 @@ test('roman numerals', () => {
   const output4 = convert('MCXIV');
   expect(output4).toBe(1114);
 });
+
+// CH-equal-sides
+function equalSides(numbers) {
+  let leftSideSum = 0;
+  let rightSideSum = numbers.reduce((acc, curr) => {
+    return (acc += curr);
+  }, 0);
+  for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i - 1]) leftSideSum += numbers[i - 1];
+    rightSideSum -= numbers[i];
+    console.log(leftSideSum, rightSideSum, i);
+    if (leftSideSum === rightSideSum) return i;
+  }
+  return -1;
+}
+
+test('equal sides', () => {
+  const output1 = equalSides([1, 2, 3, 4, 3, 2, 1]);
+  expect(output1).toBe(3);
+  const output2 = equalSides([1, 100, 50, -51, 1, 1]);
+  expect(output2).toBe(1);
+  const output3 = equalSides([20, 10, -80, 10, 10, 15, 35]);
+  expect(output3).toBe(0);
+});

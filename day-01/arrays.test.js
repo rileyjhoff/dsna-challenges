7d7247b46e068ebe6b89162894d2f328819a3da5
Riley Hoffman <rileyjhoff@gmail.com>
2022-08-15 10:48:15 -0700

    write shift function and test

--- day-01/arrays.test.js
+++ day-01/arrays.test.js
@@ -20,9 +20,9 @@ function pop(arr) {
 
 test('array pop', () => {
   const arr = [1, 2, 3, 4];
-  const lastItem = pop(arr);
+  const removedItem = pop(arr);
   expect(arr).toEqual([1, 2, 3]);
-  expect(lastItem).toBe(4);
+  expect(removedItem).toBe(4);
 });
 
 // CH04-unshift
@@ -39,4 +39,21 @@ test('array unshift', () => {
   const newLength = unshift(arr, 4);
   expect(arr).toEqual([4, 1, 2, 3]);
   expect(newLength).toBe(4);
+});
+
+// CH05-shift
+function shift(arr) {
+  const firstItem = arr[0];
+  for (let i = 0; i < arr.length - 1; i++) {
+    arr[i] = arr[i + 1];
+  }
+  arr.length = arr.length - 1;
+  return firstItem;
+}
+
+test('array shift', () => {
+  const arr = [1, 2, 3, 4];
+  const removedItem = shift(arr);
+  expect(arr).toEqual([2, 3, 4]);
+  expect(removedItem).toBe(1);
 });

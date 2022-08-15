445c0ea11b4f6bd6cfe3f791acd7c2b544c8b0e2
Riley Hoffman <rileyjhoff@gmail.com>
2022-08-15 12:58:06 -0700

    write titleCase function and test

--- day-01/lists-primitives/primitives.test.js
+++ day-01/lists-primitives/primitives.test.js
@@ -11,3 +11,19 @@ test('reverseWords', () => {
   const string = reverseWords('alchemy rocks gold');
   expect(string).toEqual('ymehcla skcor dlog');
 });
+
+// CH02-title-case-words
+function titleCase(sentence) {
+  const words = sentence.split(' ');
+  for (let i = 0; i < words.length; i++) {
+    const firstLetter = words[i][0].toUpperCase();
+    const restOfWord = words[i].slice(1, words[i].length).toLowerCase();
+    words[i] = firstLetter + restOfWord;
+  }
+  return words.join(' ');
+}
+
+test('titleCase', () => {
+  const string = titleCase('alchemy ROCKS goLD');
+  expect(string).toEqual('Alchemy Rocks Gold');
+});

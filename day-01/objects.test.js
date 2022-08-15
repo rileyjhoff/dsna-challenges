68ebb91e16bee9c42d1e8de66a18808a8e0cf652
Riley Hoffman <rileyjhoff@gmail.com>
2022-08-15 10:42:32 -0700

    write inventoryChecker function and test

---
+++ day-01/objects.test.js
@@ -0,0 +1,22 @@
+function inventoryChecker(inventory, item) {
+  if (inventory[item.itemName] > 5) {
+    return 'plenty of inventory';
+  } else return `running low on ${item.itemName}`;
+}
+
+test('inventory checker', () => {
+  const inventory = {
+    eggs: 20,
+    bananas: 10,
+  };
+  const lowItem = {
+    itemName: 'grapes',
+  };
+  const plentyItem = {
+    itemName: 'eggs',
+  };
+  const inventoryLowCheck = inventoryChecker(inventory, lowItem);
+  const inventoryPlentyCheck = inventoryChecker(inventory, plentyItem);
+  expect(inventoryLowCheck).toEqual('running low on grapes');
+  expect(inventoryPlentyCheck).toEqual('plenty of inventory');
+});

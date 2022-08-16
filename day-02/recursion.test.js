0b891d62d8366c3f3776ddf238d432556b3d7828
Riley Hoffman <rileyjhoff@gmail.com>
2022-08-16 14:14:53 -0700

    solve largest even challenge

--- day-02/recursion.test.js
+++ day-02/recursion.test.js
@@ -32,3 +32,41 @@ test('fibonacci', () => {
   const output8 = fib(8);
   expect(output8).toEqual(21);
 });
+
+// CH-largest-even
+function largestEven(nums) {
+  let first, next, rest;
+  if (nums.length >= 3) {
+    [first, next, ...rest] = nums;
+    return first % 2 !== 0 && next % 2 !== 0
+      ? largestEven([...rest])
+      : first % 2 === 0
+      ? next % 2 === 0
+        ? first >= next
+          ? largestEven([first, ...rest])
+          : largestEven([next, ...rest])
+        : largestEven([first, ...rest])
+      : largestEven([next, ...rest]);
+  }
+  if (nums.length === 2) {
+    [first, next] = nums;
+    return first % 2 !== 0 && next % 2 !== 0
+      ? -1
+      : first % 2 === 0
+      ? next % 2 === 0
+        ? first >= next
+          ? first
+          : next
+        : first
+      : next;
+  }
+}
+
+test('largest even', () => {
+  const output1 = largestEven([3, 7, 2, 1, 7, 9, 10, 13]);
+  expect(output1).toEqual(10);
+  const output2 = largestEven([1, 3, 5, 7]);
+  expect(output2).toEqual(-1);
+  const output3 = largestEven([0, 19, 18973623]);
+  expect(output3).toEqual(0);
+});

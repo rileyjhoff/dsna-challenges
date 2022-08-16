2a3fd5862cb4606594209c02b90d96a7ffd053c9
Riley Hoffman <rileyjhoff@gmail.com>
2022-08-16 10:42:54 -0700

    write every function and test

--- day-02/functions.test.js
+++ day-02/functions.test.js
@@ -1,16 +1,20 @@
-// CH-map
-function map(arr, callback) {
+// CH-every
+function every(arr, predicate) {
   for (let i = 0; i < arr.length; i++) {
-    arr[i] = callback(arr[i]);
+    if (!predicate(arr[i])) return false;
   }
-  return arr;
+  return true;
 }
 
-test('array map', () => {
-  const arr = [1, 6, 5];
-  const callback = (n) => n ** 2;
-  const mappedArr = map(arr, callback);
-  expect(mappedArr).toEqual([1, 36, 25]);
+test('array every', () => {
+  const arr1 = [2, 4, 6];
+  const predicate1 = (n) => n % 2 === 0;
+  const everyArr1 = every(arr1, predicate1);
+  expect(everyArr1).toBe(true);
+  const arr2 = [1, 2, 3];
+  const predicate2 = (n) => n % 2 === 0;
+  const everyArr2 = every(arr2, predicate2);
+  expect(everyArr2).toBe(false);
 });
 
 // CH-filter
@@ -29,4 +33,19 @@ test('array filter', () => {
   const predicate = (n) => n % 2 === 0;
   const filteredArr = filter(arr, predicate);
   expect(filteredArr).toEqual([2, 6]);
+});
+
+// CH-map
+function map(arr, callback) {
+  for (let i = 0; i < arr.length; i++) {
+    arr[i] = callback(arr[i]);
+  }
+  return arr;
+}
+
+test('array map', () => {
+  const arr = [1, 6, 5];
+  const callback = (n) => n ** 2;
+  const mappedArr = map(arr, callback);
+  expect(mappedArr).toEqual([1, 36, 25]);
 });

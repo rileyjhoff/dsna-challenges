415496548cae158174a09d07f23762c770bcb73d
Riley Hoffman <rileyjhoff@gmail.com>
2022-08-16 13:18:39 -0700

    solve fibonacci challenge

--- day-02/recursion.test.js
+++ day-02/recursion.test.js
@@ -14,3 +14,21 @@ test('digits sum root', () => {
   const output3 = rootDigit(999888777);
   expect(output3).toEqual(9);
 });
+
+// CH-fibonacci
+function fib(n) {
+  return n === 0 ? 0 : n === 1 ? 1 : fib(n - 1) + fib(n - 2);
+}
+
+test('fibonacci', () => {
+  const output0 = fib(0);
+  expect(output0).toEqual(0);
+  const output1 = fib(1);
+  expect(output1).toEqual(1);
+  const output2 = fib(2);
+  expect(output2).toEqual(1);
+  const output6 = fib(6);
+  expect(output6).toEqual(8);
+  const output8 = fib(8);
+  expect(output8).toEqual(21);
+});

675ba182d9c064805b60d2ca6e4e2daaa586a7d1
Riley Hoffman <rileyjhoff@gmail.com>
2022-08-16 10:38:22 -0700

    write map function and test. refactor filter function to set element by index instead of using .push to add to the new array

--- day-02/functions.test.js
+++ day-02/functions.test.js
@@ -1,8 +1,24 @@
+// CH-map
+function map(arr, callback) {
+  for (let i = 0; i < arr.length; i++) {
+    arr[i] = callback(arr[i]);
+  }
+  return arr;
+}
+
+test('array map', () => {
+  const arr = [1, 6, 5];
+  const callback = (n) => n ** 2;
+  const mappedArr = map(arr, callback);
+  expect(mappedArr).toEqual([1, 36, 25]);
+});
+
+// CH-filter
 function filter(arr, predicate) {
   const filteredArr = [];
   for (let i = 0; i < arr.length; i++) {
     if (predicate(arr[i])) {
-      filteredArr.push(arr[i]);
+      filteredArr[filteredArr.length] = arr[i];
     }
   }
   return filteredArr;
